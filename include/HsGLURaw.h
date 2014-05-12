b368afdc545d65b42b2fffa870b88e310f51538b
Jason Dagit <dagit@galois.com>
2014-05-11 19:46:50 -0700

    dumb down our CPP so that it works in both traditional and ansi modes

--- include/HsGLURaw.h
+++ include/HsGLURaw.h
@@ -15,19 +15,21 @@
 #ifndef HSGLURAW_H
 #define HSGLURAW_H
 
+#define HASH #
+
 /* NOTE: The macro must immediately start with the foreign declaration,
    otherwise the magic mangler (hack_foreign) in the Hugs build system
    doesn't recognize it. */
-#define API_ENTRY_INTERNAL(_entry,_ty,_safety)						\
-foreign import CALLCONV _safety "dynamic" dyn_/**/_entry :: Graphics.Rendering.GLU.Raw.APIEntry.Invoker (_ty) ; \
+#define API_ENTRY_INTERNAL(dyn_entry, ptr_entry, _entry,_ty,_safety)						\
+foreign import CALLCONV _safety "dynamic" dyn_entry :: Graphics.Rendering.GLU.Raw.APIEntry.Invoker (_ty) ; \
 _entry :: (_ty) ; \
-_entry = dyn_/**/_entry ptr_/**/_entry ; \
-ptr_/**/_entry :: FunPtr a ; \
-ptr_/**/_entry = unsafePerformIO (Graphics.Rendering.GLU.Raw.APIEntry.getAPIEntry "_entry") ; \
-{-# NOINLINE ptr_/**/_entry #-}
+_entry = dyn_entry ptr_entry ; \
+ptr_entry :: FunPtr a ; \
+ptr_entry = unsafePerformIO (Graphics.Rendering.GLU.Raw.APIEntry.getAPIEntry "_entry") ; \
+{-HASH NOINLINE ptr_entry HASH-}
 
-#define API_ENTRY(_entry,_ty) API_ENTRY_INTERNAL(_entry,_ty,unsafe)
+#define API_ENTRY(dyn_entry, ptr_entry, _entry,_ty) API_ENTRY_INTERNAL(dyn_entry, ptr_entry, _entry,_ty,unsafe)
 
-#define API_ENTRY_SAFE(_entry,_ty) API_ENTRY_INTERNAL(_entry,_ty,safe)
+#define API_ENTRY_SAFE(dyn_entry, ptr_entry, _entry,_ty) API_ENTRY_INTERNAL(dyn_entry, ptr_entry, _entry,_ty,safe)
 
 #endif
