150f606271b75d64aec0ee105149c7d352dfc099
Sven Panne <svenpanne@gmail.com>
2014-05-17 14:56:59 +0200

    Don't use K&R stringification.

--- include/HsGLURaw.h
+++ include/HsGLURaw.h
@@ -20,16 +20,16 @@
 /* NOTE: The macro must immediately start with the foreign declaration,
    otherwise the magic mangler (hack_foreign) in the Hugs build system
    doesn't recognize it. */
-#define API_ENTRY_INTERNAL(_dyn_entry,_ptr_entry,_entry,_ty,_safety) \
+#define API_ENTRY_INTERNAL(_dyn_entry,_ptr_entry,_str_entry,_entry,_ty,_safety) \
 foreign import CALLCONV _safety "dynamic" _dyn_entry :: Graphics.Rendering.GLU.Raw.APIEntry.Invoker (_ty) ; \
 _entry :: (_ty) ; \
 _entry = _dyn_entry _ptr_entry ; \
 _ptr_entry :: FunPtr a ; \
-_ptr_entry = unsafePerformIO (Graphics.Rendering.GLU.Raw.APIEntry.getAPIEntry "_entry") ; \
+_ptr_entry = unsafePerformIO (Graphics.Rendering.GLU.Raw.APIEntry.getAPIEntry _str_entry) ; \
 {-HASH NOINLINE _ptr_entry HASH-}
 
-#define API_ENTRY(_dyn_entry,_ptr_entry,_entry,_ty) API_ENTRY_INTERNAL(_dyn_entry,_ptr_entry,_entry,_ty,unsafe)
+#define API_ENTRY(_dyn_entry,_ptr_entry,_str_entry,_entry,_ty) API_ENTRY_INTERNAL(_dyn_entry,_ptr_entry,_str_entry,_entry,_ty,unsafe)
 
-#define API_ENTRY_SAFE(_dyn_entry,_ptr_entry,_entry,_ty) API_ENTRY_INTERNAL(_dyn_entry,_ptr_entry,_entry,_ty,safe)
+#define API_ENTRY_SAFE(_dyn_entry,_ptr_entry,_str_entry,_entry,_ty) API_ENTRY_INTERNAL(_dyn_entry,_ptr_entry,_str_entry,_entry,_ty,safe)
 
 #endif
