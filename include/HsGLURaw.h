87716f8efd446f7ce3113be66db7672bb95c4927
Sven Panne <svenpanne@gmail.com>
2014-05-17 12:55:14 +0200

    Fixed incorrect spaces in macro expansion. Consistently use a _ prefix for macro arguments.

--- include/HsGLURaw.h
+++ include/HsGLURaw.h
@@ -20,16 +20,16 @@
 /* NOTE: The macro must immediately start with the foreign declaration,
    otherwise the magic mangler (hack_foreign) in the Hugs build system
    doesn't recognize it. */
-#define API_ENTRY_INTERNAL(dyn_entry, ptr_entry, _entry,_ty,_safety)						\
-foreign import CALLCONV _safety "dynamic" dyn_entry :: Graphics.Rendering.GLU.Raw.APIEntry.Invoker (_ty) ; \
+#define API_ENTRY_INTERNAL(_dyn_entry,_ptr_entry,_entry,_ty,_safety) \
+foreign import CALLCONV _safety "dynamic" _dyn_entry :: Graphics.Rendering.GLU.Raw.APIEntry.Invoker (_ty) ; \
 _entry :: (_ty) ; \
-_entry = dyn_entry ptr_entry ; \
-ptr_entry :: FunPtr a ; \
-ptr_entry = unsafePerformIO (Graphics.Rendering.GLU.Raw.APIEntry.getAPIEntry "_entry") ; \
-{-HASH NOINLINE ptr_entry HASH-}
+_entry = _dyn_entry _ptr_entry ; \
+_ptr_entry :: FunPtr a ; \
+_ptr_entry = unsafePerformIO (Graphics.Rendering.GLU.Raw.APIEntry.getAPIEntry "_entry") ; \
+{-HASH NOINLINE _ptr_entry HASH-}
 
-#define API_ENTRY(dyn_entry, ptr_entry, _entry,_ty) API_ENTRY_INTERNAL(dyn_entry, ptr_entry, _entry,_ty,unsafe)
+#define API_ENTRY(_dyn_entry,_ptr_entry,_entry,_ty) API_ENTRY_INTERNAL(_dyn_entry,_ptr_entry,_entry,_ty,unsafe)
 
-#define API_ENTRY_SAFE(dyn_entry, ptr_entry, _entry,_ty) API_ENTRY_INTERNAL(dyn_entry, ptr_entry, _entry,_ty,safe)
+#define API_ENTRY_SAFE(_dyn_entry,_ptr_entry,_entry,_ty) API_ENTRY_INTERNAL(_dyn_entry,_ptr_entry,_entry,_ty,safe)
 
 #endif
